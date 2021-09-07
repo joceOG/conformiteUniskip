28c30f9b5c2fbab3ac07b018dbc17df216e0560d
saturne <saturne.dev12@gmail.com>
2021-09-07 17:45:09 +0000

    system de staper OK

--- sscript.js
+++ sscript.js
@@ -1,10 +1,11 @@
-var boxs = document.querySelectorAll('.identite');
-var i =0;
+//var boxs = document.querySelectorAll('.identite');
+/*var i =0;
 var next = document.querySelector('#next');
 var last = document.querySelector('#last');
 
 next.addEventListener('click',async()=>{
     //alert('next');
+   
     if (i < boxs.length-1) {
      
      boxs[i].id = "masque";
@@ -19,4 +20,48 @@ last.addEventListener('click',async()=>{
      i--;
      boxs[i].id = "plain";
     }      
-},false);
+},false);*/
+var control = new Map();
+var boxs = new Map();
+
+var next = document.querySelectorAll('#next');
+var last = document.querySelectorAll('#last');
+
+
+next.forEach((bott)=>{
+
+   boxs.set(bott.name, document.querySelectorAll(`.${bott.name}`));
+   control.set(bott.name,0);
+
+   bott.addEventListener('click',()=>{
+
+       //alert( boxs.get(bott.name).length );
+
+       if (control.get(bott.name) < boxs.get(bott.name).length-1) {
+           boxs.get(bott.name)[control.get(bott.name)].id = "masque";
+           control.set(bott.name,control.get(bott.name)+1);
+           boxs.get(bott.name)[control.get(bott.name)].id = "plain";
+       }
+
+   },false);
+});
+
+last.forEach((bott)=>{
+
+    boxs.set(bott.name, document.querySelectorAll(`.${bott.name}`));
+    control.set(bott.name,0);
+ 
+    bott.addEventListener('click',()=>{
+ 
+        //alert( boxs.get(bott.name).length );
+ 
+        if (control.get(bott.name) > 0) {
+            boxs.get(bott.name)[control.get(bott.name)].id = "masque";
+            control.set(bott.name,control.get(bott.name)-1);
+            boxs.get(bott.name)[control.get(bott.name)].id = "plain";
+        }
+ 
+    },false);
+ });
+
+
